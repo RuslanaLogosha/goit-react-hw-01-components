c66628bbdc3042b002fcbc825f9c936a7d7d9079
Ruslana Logosha <ruslanalogosha@gmail.com>
2020-11-26 00:49:22 +0200

    TransactionsHistory initial js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import statisticalData from './components/Statistics/statistical-data.json';
 import FriendList from './components/FriendList/friendList';
 import friends from './components/FriendList/friends.json';
 
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
+import transactions from './components/TransactionHistory/transactions.json';
 
 
 export default function App() {
@@ -22,6 +24,7 @@ export default function App() {
       <Statistics title="Upload stats" stats={statisticalData} />
       <Statistics stats={statisticalData} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div> 
 
   );
